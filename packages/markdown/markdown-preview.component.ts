638f0a7c7fbef4e30bf8e157ed6d27da9c686f6c
palenge <palenge@users.noreply.github.com>
2022-09-19 10:56:48 +0000

    add options

--- packages/markdown/markdown-preview.component.ts
+++ packages/markdown/markdown-preview.component.ts
@@ -13,7 +13,17 @@ declare var Vditor: any;
 export class NuMarkdownPreviewComponent extends NuMarkdownBaseComponent {
   protected init(): void {
     this.ngZone.runOutsideAngular(async () => {
-      await Vditor.preview(this.el.nativeElement, this._value);
+      const options = {
+        value: this._value,
+        cache: {
+          enable: false,
+        },
+        mode: 'sv',
+        minHeight: 350,
+        ...this.config?.defaultOptions,
+        ...this.options,
+      };
+      await Vditor.preview(this.el.nativeElement, this._value, options);
       this.ngZone.run(() => this.ready.emit(this.el.nativeElement.innerHTML));
     });
   }
